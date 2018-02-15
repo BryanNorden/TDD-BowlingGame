6bb94cfef69722b579996c6b1bf28bf96ca7405e
Bryan Norden <bryan.norden@yahoo.com>
2018-02-15 13:38:30 -0800

    Bowling Game Logic completed using TDD

--- BowlingGameTests/BowlingGameTests.swift
+++ BowlingGameTests/BowlingGameTests.swift
@@ -11,26 +11,70 @@ import XCTest
 
 class BowlingGameTests: XCTestCase {
     
+    var game: Game!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        game = Game()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        game = nil
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testGutterGame() {
+        rollMany(pins: 0, times: 20)
+        XCTAssertEqual(game.score(), 0)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testAllOnes() {
+        rollMany(pins: 1, times: 20)
+        XCTAssertEqual(game.score(), 20)
+    }
+    
+    func testOneSpare() {
+        rollSpare()
+        game.roll(3)
+        rollMany(pins: 0, times: 17)
+        XCTAssertEqual(game.score(), 16)
+    }
+    
+    func testOneStrike() {
+        rollStrike()
+        game.roll(3)
+        game.roll(4)
+        rollMany(pins: 0, times: 16)
+        XCTAssertEqual(game.score(), 24)
+    }
+    
+    func testOneTurkey() {
+        rollStrike()
+        rollStrike()
+        rollStrike()
+        XCTAssertEqual(game.score(), 60)
+    }
+    
+    func testPerfectGame() {
+        rollMany(pins: 10, times: 12)
+        XCTAssertEqual(game.score(), 300)
+    }
+    
+    // MARK: - Private
+    
+    private func rollMany(pins: Int, times: Int) {
+        for _ in 1...times {
+            game.roll(pins)
         }
+    }
+    
+    private func rollSpare() {
+        game.roll(5)
+        game.roll(5)
+    }
+    
+    private func rollStrike() {
+        game.roll(10)
     }
     
 }
